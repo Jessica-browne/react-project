45749f51d5bee4dff41d87bb2cc13256cb3090c4
Jessica <20101992@mail.wit.ie>
2024-11-23 17:18:38 +0000

    Add to favourites logic completed; Dummy favourites page.

--- movies/src/pages/movieDetailsPage.js
+++ movies/src/pages/movieDetailsPage.js
@@ -50,24 +50,24 @@ const MoviePage = (props) => {
                 justifyContent: "space-around",
               }}>
                   <ImageList
-    sx={{
-      height: "100vh",
-    }}
-    cols={1}
-  >
-    {images.map((image) => (
-      <ImageListItem
-      key={image.file_path}
-      cols={1}
-      >
-        <img
-          src={`https://image.tmdb.org/t/p/w500/${image.file_path}`}
-          alt={image.file_path}
-        />
-      </ImageListItem>
-    ))}
-  </ImageList>
-              </div>
+                     sx={{
+                    height: "100vh",
+                    }}
+                    cols={1}
+                >
+                    {images.map((image) => (
+                    <ImageListItem
+                    key={image.file_path}
+                    cols={1}
+                    >
+                        <img
+                        src={`https://image.tmdb.org/t/p/w500/${image.file_path}`}
+                        alt={image.file_path}
+                        />
+                    </ImageListItem>
+                    ))}
+                </ImageList>
+                 </div>
             </Grid>
             <Grid size={{xs: 9}}>
               <MovieDetails movie={movie} />
