3bf27b31d7484066bcb84a57ec5c4ffd81071c62
Jessica <20101992@mail.wit.ie>
2024-11-23 19:38:30 +0000

    Added new review support.

--- movies/src/components/movieCard/index.js
+++ movies/src/components/movieCard/index.js
@@ -18,7 +18,7 @@ import Avatar from '@mui/material/Avatar';
 
 
 
-export default function MovieCard({ movie, action })  { 
+export default function MovieCard({ movie, action }) {
   const { favorites, addToFavorites } = useContext(MoviesContext);
 
   if (favorites.find((id) => id === movie.id)) {
@@ -73,15 +73,16 @@ export default function MovieCard({ movie, action })  {
         </Grid>
       </CardContent>
       <CardActions disableSpacing>
-       <IconButton aria-label="add to favorites" onClick={handleAddToFavorite}>
-         <FavoriteIcon color="primary" fontSize="large" />
-        </IconButton>
-        <Link to={`/movies/${movie.id}`}>
-          <Button variant="outlined" size="medium" color="primary">
-            More Info ...
-          </Button>
-        </Link>
-      </CardActions>
+      
+      {action(movie)}
+    
+      <Link to={`/movies/${movie.id}`}>
+        <Button variant="outlined" size="medium" color="primary">
+          More Info ...
+        </Button>
+      </Link>
+      
+    </CardActions>
     </Card>
   );
 }
