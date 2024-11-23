25a893d942ad0fcff1e7c0b33c79f6be28e0eb3d
Jessica <20101992@mail.wit.ie>
2024-11-23 20:25:37 +0000

    Added black upcoming page

--- movies/src/api/tmdb-api.js
+++ movies/src/api/tmdb-api.js
@@ -86,3 +86,23 @@ export const getMovie = (args) => {
       throw error
    });
   };
+
+  export const getUpcomingMovies = ({ queryKey }) => {
+    const [, idPart] = queryKey;
+    const { id } = idPart;
+
+    return fetch(
+      `https://api.themoviedb.org/3/movie/upcoming?api_key=${process.env.REACT_APP_TMDB_KEY}`
+    ).then( (response) => {
+      if (!response.ok) {
+        return response.json().then((error) => {
+          throw new Error(error.status_message || "Something went wrong");
+        });
+      }
+      return response.json();
+    })
+    .catch((error) => {
+      throw error
+   });
+  };
+
